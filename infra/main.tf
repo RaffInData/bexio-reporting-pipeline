550e3bb0f7c3d005d06056f07279354d9537c222
raffaelschoch <schochraffael@gmx.ch>
2025-05-04 14:54:45 +0200

    feat(222): add azure function with app service plan

--- infra/main.tf
+++ infra/main.tf
@@ -20,3 +20,36 @@ resource "azurerm_storage_account" "this" {
     environment = "dev"
   }
 }
+
+resource "azurerm_app_service_plan" "this" {
+  name                = "bexio-plan"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "FunctionApp"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+
+  tags = {
+    project     = "bexio-reporting"
+    environment = "dev"
+  }
+}
+
+resource "azurerm_function_app" "this" {
+  name                       = "bexio-function-app"
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  app_service_plan_id        = azurerm_app_service_plan.this.id
+  storage_account_name       = azurerm_storage_account.this.name
+  storage_account_access_key = azurerm_storage_account.this.primary_access_key
+  version                    = "~4"
+
+  tags = {
+    project     = "bexio-reporting"
+    environment = "dev"
+  }
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,7 @@ output "storage_account_name" {
   description = "The name of the created Storage Account"
   value       = azurerm_storage_account.this.name
 }
+
+output "function_app_name" {
+  value = azurerm_function_app.this.name
+}
